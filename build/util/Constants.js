11cfa891317375f6c9c4e8e68429e04bcafe6454
radhey113 <radheyg11@gmail.com>
2018-07-27 23:21:34 +0530

    files removed

--- build/util/Constants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    API: {
-        UserRoutes: {
-            user: "/api/v1/user"
-        }
-    }
-};
